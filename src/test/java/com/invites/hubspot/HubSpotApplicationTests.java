e5c630c9fd0355e929627a86587c8f8cba2af7b6
Weverton de Souza Castanho <weverton.castanho@AIR-DE-WEVERTON.station>
2021-06-21 13:15:16 +0100

    HubSpot - First commit

---
+++ src/test/java/com/invites/hubspot/HubSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invites.hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
